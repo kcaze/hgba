aad44a5dd7d508f756eea6d081d2c9dbc4028478
Herman Chau <hchau@stanford.edu>
2016-02-29 06:00:33 -0800

    Finished implementing what I hope are clean getters and setters now.

--- src/Bits.hs
+++ src/Bits.hs
@@ -1,55 +1,34 @@
-module Bits ( Bits'
-            , Num'
-            ) where
+module Bits where
 
-import Control.Applicative
-import Data.Bits as B
+import Data.Bits
 import Data.Int
 import Data.Word
 
--- These typeclass instances are general but they're just used for Get.
-class Num' a where
-  (.+.) :: a -> a -> a
-  (.*.) :: a -> a -> a
+(.^.) :: Word32 -> Word32 -> Word32
+(.^.) = xor
+(.<<.) :: Word32 -> Int -> Word32
+(.<<.) = flip logicalShiftL
+(.>>.) :: Word32 -> Int -> Word32
+(.>>.) = flip logicalShiftR
+(.>>>.) :: Word32 -> Int -> Word32
+(.>>>.) = flip arithmeticShiftR
+(.<@<.) :: Word32 -> Int -> Word32
+(.<@<.) = rotateL
+(.>@>.) :: Word32 -> Int -> Word32
+(.>@>.) = rotateR
 
-instance Num b => Num' (a -> b) where
-  (.+.) = liftA2 (+)
-  (.*.) = liftA2 (*)
+logicalShiftL :: Int -> Word32 -> Word32
+logicalShiftR :: Int -> Word32 -> Word32
+arithmeticShiftR :: Int -> Word32 -> Word32
+logicalShiftL n w = fromIntegral $ (fromIntegral w :: Word32) `shiftL` n
+logicalShiftR n w = fromIntegral $ (fromIntegral w :: Word32) `shiftR` n
+arithmeticShiftR n w = fromIntegral $ (fromIntegral w :: Int32) `shiftR` n
+rotateL' :: Int -> Word32 -> Word32
+rotateL' = flip rotateL
+rotateR' :: Int -> Word32 -> Word32
+rotateR' = flip rotateR
 
-class Bits' a where
-  (.&.) :: a -> a -> a    -- AND
-  (.|.) :: a -> a -> a    -- OR
-  (.^.) :: a -> a -> a    -- XOR
-  (.~.) :: a -> a         -- NOT
-  (.>>.) :: a -> a -> a   -- LSR
-  (.<<.) :: a -> a -> a   -- LSL
-  (.>>>.) :: a -> a -> a  -- ASR
-  (.>@>.) :: a -> a -> a  -- ROR
-  (.<@<.) :: a -> a -> a  -- ROL
-
-instance (Integral b, Bits b) => Bits' (a -> b) where
-  (.&.) = liftA2 (B..&.)
-  (.|.) = liftA2 (B..|.)
-  (.^.) = liftA2 xor
-  (.~.) = liftA complement
-  (.>>.) = liftA2 logicalShiftL
-  (.<<.) = liftA2 logicalShiftR
-  (.>>>.) = liftA2 arithmeticShiftR
-  (.>@>.) = liftA2 rotateR'
-  (.<@<.) = liftA2 rotateL'
-
-logicalShiftL :: (Integral a, Bits a) => a -> a -> a
-logicalShiftR :: (Integral a, Bits a) => a -> a -> a
-arithmeticShiftR :: (Integral a, Bits a) => a -> a -> a
-rotateR' :: (Integral a, Bits a) => a -> a -> a
-rotateL' :: (Integral a, Bits a) => a -> a -> a
-x `logicalShiftL` y = fromIntegral $
-                      (fromIntegral x :: Word32) `shiftL` (fromIntegral y)
-x `logicalShiftR` y = fromIntegral $
-                      (fromIntegral x :: Word32) `shiftR` (fromIntegral y)
-x `arithmeticShiftR` y = fromIntegral $
-                         (fromIntegral x :: Int32) `shiftR` (fromIntegral y)
-x `rotateR'` y = fromIntegral $
-                 (fromIntegral x :: Int32) `rotateR` (fromIntegral y)
-x `rotateL'` y = fromIntegral $
-                 (fromIntegral x :: Int32) `rotateL` (fromIntegral y)
+bitRange :: Int -> Int -> Word32 -> Word32
+bitRange low high w
+  | low > high = 0
+  | otherwise = w .<<. (31 - high) .>>. (31 - (high - low))
--- src/Mutable.hs
+++ src/Mutable.hs
@@ -1,5 +1,3 @@
 module Mutable where
 
 type Get s a = s -> a
-type Set s a = s -> Get s a -> s
-data Mutable s a = Mutable { get :: Get s a, set :: Set s a}
