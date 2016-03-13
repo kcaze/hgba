5b2b3a55130c71f4b55a627c86f4b4c337ee66e1
Herman Chau <hchau@stanford.edu>
2016-03-12 23:15:24 -0800

    Finally, graphics.

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,6 @@ module Main where
 import Data.Bits
 import Data.Word
 import qualified Data.ByteString as B
-import qualified Data.Map.Strict as Map
 import Numeric
 import System.IO
 import SDL hiding (get)
@@ -13,6 +12,7 @@ import Linear.Affine
 
 import CPU
 import Execute
+import GPU
 import Imperative
 import Types
 
@@ -23,9 +23,9 @@ prompt :: CPU -> String
 prompt cpu = "0x" ++ pad8 (showHex (cpu_r15 cpu) "") ++ "> "
 
 main = do
---  SDL.initializeAll
---  window <- createWindow "hgba" defaultWindow { windowInitialSize = V2 240 160 }
---  renderer <- createRenderer window (-1) defaultRenderer
+  SDL.initializeAll
+  window <- createWindow "hgba" defaultWindow { windowInitialSize = V2 240 160 }
+  renderer <- createRenderer window (-1) defaultRenderer
 
   hSetBuffering stdin NoBuffering
   hSetBuffering stdout NoBuffering
@@ -33,38 +33,44 @@ main = do
   bios <- B.readFile "bios.bin"
   game <- B.readFile "game.bin"
   let cpu = run (loadBIOS bios .>> loadGame game) powerUp
-  loop cpu
+  debugLoop cpu renderer
 
-loop :: CPU -> IO ()
-loop cpu = do
-  {--
-  rendererDrawColor renderer $= V4 0 0 0 255
-  clear renderer
-  rendererDrawColor renderer $= V4 255 255 255 255
-  drawPoint renderer (P $ V2 24 24)
-  present renderer
-  --}
+loop :: CPU -> Renderer -> IO ()
+loop cpu renderer = do
+  if cpu_cycles cpu `mod` 70224 == 0 then render cpu renderer else return ()
+  ready <- hReady stdin
+  c <- if ready then hGetChar stdin else return ' '
+  if c == 'd'
+    then debugLoop (run step cpu) renderer
+    else loop (run step cpu) renderer
+
+debugLoop :: CPU -> Renderer -> IO ()
+debugLoop cpu renderer = do
+  render cpu renderer
 
   putStr $ prompt cpu
   c <- hGetChar stdin
   case c of
+    'c' -> do putStrLn "Continuing..."
+              loop (run step cpu) renderer
     'q' -> do putStrLn "Goodbye."
               return ()
     'b' -> do cpu' <- setBreakpoint cpu
-              loop cpu'
+              debugLoop cpu' renderer
     ' ' -> do putStr "\n"
-              loop (run step cpu)
+              debugLoop (run step cpu) renderer
     'i' -> do putStrLn $ show cpu
-              loop cpu
+              debugLoop cpu renderer
     'w' -> do viewMemoryWord cpu
-              loop cpu
+              debugLoop cpu renderer
     _   -> do putStrLn $ "Commands:\n" ++
+                         "  'c' to continue\n" ++
                          "  'q' to quit\n" ++
                          "  spacebar to step\n" ++
                          "  'i' to view cpu\n" ++
                          "  'b' to break at an address\n" ++
                          "  'w' to view a word in memory"
-              loop cpu
+              debugLoop cpu renderer
 
 setBreakpoint :: CPU -> IO CPU
 setBreakpoint cpu = do
--- src/GPU.hs
+++ src/GPU.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE BinaryLiterals #-}
 module GPU where
 
+import Control.Applicative
 import Data.Bits
 import Data.Word
+import Numeric
+import SDL hiding (get)
+import Linear
+import Linear.Affine
 
 import CPU
 import Imperative
@@ -19,6 +24,10 @@ writeAddress a f c = c `seq` c { cpu_memory = memory }
 rDISPCNT = 0x04000000
 rDISPSTAT = 0x04000004
 rVCOUNT = 0x04000006
+addrOAM = 0x07000000
+addrOBJTiles = 0x06010000
+addrBGPalette = 0x05000000
+addrOBJPalette = 0x05000200
 
 data VideoMode = Mode0 | Mode1 | Mode2 | Mode3 | Mode4 | Mode5
   deriving (Eq, Show)
@@ -75,3 +84,90 @@ updateLCD = fromFunction (\cpu -> cpu `seq`(
     ) cpu)
 
   where inRange x l h = l <= x && x <= h
+
+getBG256Color :: Int -> CPU -> Word32
+getBG256Color n cpu = read16 (addrBGPalette + fromIntegral (2*n)) cpu
+
+getOBJ256Color :: Int -> CPU -> Word32
+getOBJ256Color n cpu = read16 (addrOBJPalette + fromIntegral (2*n)) cpu
+
+getSprite :: Int -> CPU -> Sprite
+getSprite n cpu = Sprite {
+    sprite_x = fromIntegral $ attr1 .&. 0x1FF,
+    sprite_y = fromIntegral $ attr0 .&. 0x0FF,
+    sprite_size = case (bitRange 14 15 attr0, bitRange 14 15 attr1) of
+                       (0b00, 0b00) -> (1, 1)
+                       (0b00, 0b01) -> (2, 2)
+                       (0b00, 0b10) -> (4, 4)
+                       (0b00, 0b11) -> (8, 8)
+                       (0b01, 0b00) -> (2, 1)
+                       (0b01, 0b01) -> (4, 1)
+                       (0b01, 0b10) -> (4, 2)
+                       (0b01, 0b11) -> (8, 4)
+                       (0b10, 0b00) -> (1, 2)
+                       (0b10, 0b01) -> (1, 4)
+                       (0b10, 0b10) -> (2, 4)
+                       (0b10, 0b11) -> (4, 8)
+                       _ -> error "Invalid sprite size.",
+    sprite_tile = fromIntegral $ attr2 .&. 0x3FF,
+    sprite_palette = fromIntegral $ attr2 .&. 0xF000,
+    sprite_256ColorEnabled = testBit attr0 13
+  }
+  where attr0 = read16 address cpu
+        attr1 = read16 (address + 2) cpu
+        attr2 = read16 (address + 4) cpu
+        attr3 = read16 (address + 6) cpu
+        address = addrOAM + (fromIntegral $ 8 * n)
+
+data Sprite = Sprite {
+  sprite_x :: Int,
+  sprite_y :: Int,
+  sprite_size :: (Int, Int),
+  sprite_tile :: Int,
+  sprite_palette :: Int,
+  sprite_256ColorEnabled :: Bool
+}
+
+---------------
+-- Renderers --
+---------------
+renderPixel :: Word32 -> (Int, Int) -> Renderer -> IO()
+renderPixel c (x, y) renderer = do
+  let (r,g,b) = (bitRange 0 4 c <! 3, bitRange 5 9 c <! 3, bitRange 10 14 c <! 3)
+  rendererDrawColor renderer $= V4 (fromIntegral r) (fromIntegral g) (fromIntegral b) 255
+  drawPoint renderer (P $ V2 (fromIntegral x) (fromIntegral y))
+
+render256ColorTile :: Address -> Int -> (Int, Int) -> CPU -> Renderer -> IO()
+render256ColorTile baseAddress n (x, y) cpu renderer = do
+  foldl (>>) (return ()) (map plot (liftA2 (,) [0..7] [0..7]))
+  where address = baseAddress + (fromIntegral $ n * 0x20)
+        plot (dx, dy) = do
+          let pixel = read8 (address + fromIntegral dx + 8 * fromIntegral dy) cpu
+          renderPixel (getOBJ256Color (fromIntegral pixel) cpu) (x + dx, y + dy) renderer
+
+renderSprite :: Int -> CPU -> Renderer -> IO()
+renderSprite n cpu renderer =
+  foldl (>>) (return ()) (map plot (liftA2 (,) [0..(w-1)] [0..(h-1)]))
+  where sprite = getSprite n cpu
+        (w, h) = sprite_size sprite
+        plot (tx, ty) = do
+          render256ColorTile (addrOBJTiles)
+                             (sprite_tile sprite + 2*tx + 32*ty)
+                             (sprite_x sprite + 8*tx, sprite_y sprite + 8*ty)
+                             cpu
+                             renderer
+
+renderSprites :: CPU -> Renderer -> IO()
+renderSprites cpu renderer = do
+  foldl (>>) (return ()) (map plot [0..10])
+  where plot n = renderSprite n cpu renderer
+
+render :: CPU -> Renderer -> IO()
+render cpu renderer = do
+  putStrLn "Rendering frame."
+  rendererDrawColor renderer $= V4 0 0 0 255
+  clear renderer
+
+  if get oamEnabled cpu then renderSprites cpu renderer else return ()
+
+  present renderer
